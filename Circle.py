8645171b34400244505c17f0fb5187144f8600b6
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-09-18 12:22:55 +0900

    Drill02 제출 (2019182029 이원준)

--- Circle.py
+++ Circle.py
@@ -6,10 +6,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x, y = 
+x, y = 400, 600
 
 while(True) :
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y))
 
 close_canvas()
-
-→ 1도 = 180 * 1라디안 / pi
+    
